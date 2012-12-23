54b6d999fac7d16c72da0be2b4f7c09e7c0e81a8
moregeek <gh@guerilladesigns.com>
2012-12-23 10:20:19 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectionCounter::Application.config.secret_token = '9dd09ea85cec23fd7a111d89269699a82cc7bc5075d3d72b0f23a1230ccf313230a52322191c82039da9af1f636a66c03a33bd3ef664083c674d198706cd5d5b'
