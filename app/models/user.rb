e5cac2196543edf39aba93e29cc8ef06c831a9d1
moregeek <gh@guerilladesigns.com>
2012-12-31 16:22:44 +0100

    add: usermodel

---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :approved
+  # attr_accessible :title, :body
+
+  def active_for_authentication? 
+    super && approved? 
+  end 
+
+  def inactive_message 
+    if !approved? 
+      :not_approved 
+    else 
+      super # Use whatever other message 
+    end 
+  end
+
+# @ TODO Create mailer - to get notified after a new user registerd.
+# https://github.com/plataformatec/devise/wiki/How-To%3a-Require-admin-to-activate-account-before-sign_in
+#
+#  after_create :send_admin_mail
+#  def send_admin_mail
+#    AdminMailer.new_user_waiting_for_approval(self).deliver
+#  end
+
+  def self.send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    if !recoverable.approved?
+      recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+    elsif recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
+
+end
