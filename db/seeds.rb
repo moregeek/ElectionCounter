09566845e29e66c2796668b73f3c08aaa51d6039
moregeek <gh@guerilladesigns.com>
2012-12-31 16:28:21 +0100

    datbase seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Vote Districts
+#  -> Carinthia
+["Klagenfurt", "Kaernten Ost", "Villach", "Kaernten West"].each do |vd|
+  Votedistrict.find_or_create_by_name(vd)
+end
+
+# District: 1
+["Klagenfurt am Woerthersee", "Klagenfurt Land"].each do |d|
+  Votedistrict.find(1).districts.create(name: d) 
+end
+
+# District: 2
+["Sankt Veit an der Glan", "Voelkermark", "Wolfsberg"].each do |d|
+  Votedistrict.find(2).districts.create(name: d) 
+end
+
+# District: 3
+["Villach", "Villach Land"].each do |d|
+  Votedistrict.find(3).districts.create(name: d) 
+end
+
+# District: 4
+["Feldkirchen", "Hermagor", "Spital an der Drau"].each do |d|
+  Votedistrict.find(4).districts.create(name: d) 
+end
